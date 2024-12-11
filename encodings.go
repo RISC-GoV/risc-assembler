ab6788d0ae628f6b88b1540fa2d87e1970605888
Sanegv <43825023+Sanegv@users.noreply.github.com>
2024-12-11 19:22:42 +0100

    implemented bit inversion

--- encodings.go
+++ encodings.go
@@ -18,6 +18,18 @@ type Assembler struct {
 	currentPC  int
 }
 
+func invertBits(integer uint32) uint32 {
+	var result uint32 = 0
+
+	for range 32 {
+		result |= integer & 1
+		integer >>= 1
+		result <<= 1
+	}
+
+	return result
+}
+
 func (a *Assembler) encodeRType(inst *Instruction) uint32 {
 	result := uint32(inst.opcode)
 	result |= uint32(inst.rd) << 7
@@ -25,7 +37,7 @@ func (a *Assembler) encodeRType(inst *Instruction) uint32 {
 	result |= uint32(inst.rs1) << 15
 	result |= uint32(inst.rs2) << 20
 	result |= uint32(inst.funct7) << 25
-	return result
+	return invertBits(result)
 }
 
 func (a *Assembler) encodeIType(inst *Instruction) uint32 {
@@ -34,7 +46,7 @@ func (a *Assembler) encodeIType(inst *Instruction) uint32 {
 	result |= uint32(inst.funct3) << 12
 	result |= uint32(inst.rs1) << 15
 	result |= (uint32(inst.immediate) & 0xFFF) << 20
-	return result
+	return invertBits(result)
 }
 
 func (a *Assembler) encodeSType(inst *Instruction) uint32 {
@@ -45,7 +57,7 @@ func (a *Assembler) encodeSType(inst *Instruction) uint32 {
 	result |= uint32(inst.rs1) << 15
 	result |= uint32(inst.rs2) << 20
 	result |= (imm & 0xFE0) << 20 // imm[11:5]
-	return result
+	return invertBits(result)
 }
 
 func (a *Assembler) encodeBType(inst *Instruction) uint32 {
@@ -58,14 +70,14 @@ func (a *Assembler) encodeBType(inst *Instruction) uint32 {
 	result |= uint32(inst.rs2) << 20
 	result |= ((imm >> 5) & 0x3F) << 25 // imm[10:5]
 	result |= ((imm >> 12) & 0x1) << 31 // imm[12]
-	return result
+	return invertBits(result)
 }
 
 func (a *Assembler) encodeUType(inst *Instruction) uint32 {
 	result := uint32(inst.opcode)
 	result |= uint32(inst.rd) << 7
 	result |= (uint32(inst.immediate) & 0xFFFFF000)
-	return result
+	return invertBits(result)
 }
 
 func (a *Assembler) encodeJType(inst *Instruction) uint32 {
@@ -76,5 +88,5 @@ func (a *Assembler) encodeJType(inst *Instruction) uint32 {
 	result |= ((imm >> 11) & 0x1) << 20  // imm[11]
 	result |= ((imm >> 1) & 0x3FF) << 21 // imm[10:1]
 	result |= ((imm >> 20) & 0x1) << 31  // imm[20]
-	return result
+	return invertBits(result)
 }
